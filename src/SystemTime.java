6a551f390c69ba98c008031c59f7084dd7aa0237
LamBui0510 <buitunglamvnpt@gmail.com>
2021-11-04 22:32:11 +0700

    khai bao bien

---
+++ src/SystemTime.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+import java.util.zip.DataFormatException;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is " + now);
+        System.out.println();
+    }
+}
