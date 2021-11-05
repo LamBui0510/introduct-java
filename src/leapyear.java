e4924d3a61c4e55c57410a0b4ffd88ef39e646d7
LamBui0510 <buitunglamvnpt@gmail.com>
2021-11-05 09:14:20 +0700

    PTB1

---
+++ src/leapyear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("enter a year");
+        year = sc.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + "is a leap year");
+                } else {
+                    System.out.println(year + "is not a leap year");
+                }
+            } else {
+                System.out.println(year + "is a leap year");
+            }
+        } else {
+            System.out.println(year + "is not a leap year");
+        }
+    }
+}
